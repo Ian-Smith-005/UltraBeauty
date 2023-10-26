a943a6a1c26bbf2abefc6036be075422a430e477
Ian de Smith <90592108+Ian-Smith-005@users.noreply.github.com>
2023-10-26 15:25:33 +0300

    new

--- UB_X/scripts/cookies.js
+++ UB_X/scripts/cookies.js
@@ -1,52 +1,32 @@
-// Function to set a cookie
-function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
+document.addEventListener("DOMContentLoaded", function() {
+    const cookieConsent = document.querySelector('.cookie-consent');
+    const acceptBtn = document.getElementById('accept-btn');
+    const rejectBtn = document.getElementById('reject-btn');
+    const messageDiv = document.getElementById('message');
+
+    let isMessageDisplayed = false;
+
+    // Check if the user has visited the site before
+    const hasVisitedBefore = localStorage.getItem('hasVisitedBefore');
+
+    if (!hasVisitedBefore) {
+        cookieConsent.style.display = 'block';
     }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
 
-// Function to get a cookie by name
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var cookies = document.cookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        while (cookie.charAt(0) === ' ') {
-            cookie = cookie.substring(1, cookie.length);
+    acceptBtn.addEventListener('click', function() {
+        localStorage.setItem('hasVisitedBefore', true);
+        cookieConsent.style.display = 'none';
+    });
+
+    rejectBtn.addEventListener('click', function() {
+        if (isMessageDisplayed) {
+            messageDiv.textContent = '';
+            messageDiv.classList.remove('error');
+            cookieConsent.style.display = 'none';
+        } else {
+            messageDiv.textContent = "You might have trouble accessing some services without accepting cookies.";
+            messageDiv.classList.add('error');
+            isMessageDisplayed = true;
         }
-        if (cookie.indexOf(nameEQ) === 0) {
-            return cookie.substring(nameEQ.length, cookie.length);
-        }
-    }
-    return null;
-}
-
-// Function to check if the IP cookie exists
-function ipCookieExists() {
-    return getCookie("visitedByIP") !== null;
-}
-
-// Function to display the cookie banner
-function showCookieBanner() {
-    var banner = document.getElementById("cookie-banner");
-    banner.style.display = "block";
-}
-
-// Function to handle the "Accept" button click
-function acceptCookies() {
-    setCookie("visitedByIP", "true", 365); // Set the IP cookie for 1 year
-    var banner = document.getElementById("cookie-banner");
-    banner.style.display = "none";
-}
-
-// Check if the IP cookie exists and display the banner if not
-if (!ipCookieExists()) {
-    showCookieBanner();
-}
-
-// Attach a click event listener to the "Accept" button
-document.getElementById("accept-cookies").addEventListener("click", acceptCookies);
+    });
+});
