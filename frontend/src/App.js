6f1582c8a269a069dfcd4bfa4008a1e8e2cafcfa
Ian de Smith <90592108+Ian-Smith-005@users.noreply.github.com>
2023-11-06 15:58:48 +0300

    new

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Checkout from "./components/checkout";
+function App() {
+  return (
+    <div className="App">
+      <Checkout></Checkout>
+    </div>
+  );
+}
+
+export default App;
